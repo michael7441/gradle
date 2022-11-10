865c5c62e33462ae977019bdfc336aec07899ddd
Michael Singleton <michael.singleton@vonage.com>
2022-11-10 11:15:51 +0000

    scripts

---
+++ code/run.sh
@@ -0,0 +1,6 @@
+
+
+clear && printf "\e[3J"
+$HOME/.nvm/versions/node/v16.18.0/bin/node /Users/msingleton/repos/gradle_dependancy_update_bot/code/index.js
+
+
---
+++ code/watch.sh
@@ -0,0 +1,3 @@
+
+watch ./run.sh && c && node
+
